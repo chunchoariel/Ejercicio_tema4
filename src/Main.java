26ab21f174e5d7e814c3309e89029da40efaeaed
chunchoariel <ariel.mariangel.escobar@gmail.com>
2022-10-05 13:18:47 -0300

    Ejercicios tema 4

---
+++ src/Main.java
@@ -0,0 +1,65 @@
+package com.open_bootcamp;
+
+//public class Main { //Uso de if
+//    public static void main(String[] args) {
+//        int numeroIf = 5;
+//        if ( numeroIf > 0) {
+//            System.out.println("El número es Positivo");
+//        } else if (numeroIf == 0) {
+//            System.out.println("El número CERO es Neutro");
+//        } else {
+//            System.out.println("El número es Negativo");
+//        }
+//   }
+//}
+
+//public class Main { //Uso de While
+//    public static void main(String[] args) {
+//        int numeroWhile = 1;
+//        while (numeroWhile < 3) {
+//            System.out.println(numeroWhile);
+//            numeroWhile = numeroWhile + 1;
+//        }
+//    }
+//}
+
+
+//public class Main { //Uso de DO While
+//    public static void main(String[] args) {
+//        int numeroDoWhile = 1;
+//        do {
+//            System.out.println(numeroDoWhile);
+//            numeroDoWhile = numeroDoWhile + 1;
+//        } while (numeroDoWhile < 3);
+//    }
+//}
+
+//public class Main { //Uso de for
+//    public static void main(String[] args) {
+//        for (int numeroFor = 0; numeroFor <= 3; numeroFor++){
+//            System.out.println(numeroFor);}
+//        }
+//    }
+
+
+//public class Main { //Uso de switch
+//    public static void main(String[] args) {
+//        var estacion = "PRIMAVERA";
+//        switch (estacion) {
+//            case "VERANO":
+//                System.out.println("La estación del año es Verano");
+//                break;
+//            case "OTOÑO":
+//                System.out.println("La estación del año es Otoño");
+//                break;
+//            case "INVIERNO":
+//               System.out.println("La estación del año es Invierno");
+//                break;
+//            case "PRIMAVERA":
+//                System.out.println("La estación del año es Primavera");
+//                break;
+//            default:
+//                System.out.println("Debes indicar una de las 4 estaciones: VERANO-OTOÑO-INVIERNO-PRIMAVERA");
+//        }
+//    }
+//}
